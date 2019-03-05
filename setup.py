9610d5c5a7e8085b75f20be219aebe01b9fad182
moka <ch.ems6666@gmail.com>
2019-03-05 18:50:24 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+
+with open('README.md', encoding='utf-8') as f:
+	long_description = f.read()
+
 setup(
-    name="nhentai-api",
-    version="0.1",
-    packages=find_packages(),
-	author="moka"
+	name="nhentai-api",
+	version="1.0.1",
+	packages=find_packages(),
+	author="moka",
+	long_description=long_description,
+	long_description_content_type='text/x-rst',
+	url='github.com/moe-ka/nhentai-api-wrapper'
 )
